b06816facf36840f7eb42e6fe8bbfa7e62f264d0
Alejandro Ladislao Rodriguez Perez <arodpere@everis.com>
2020-02-26 16:00:27 +0100

    Added interfaces for User and need its, also Added a getUsers Request in service

---
+++ src/app/core/interfaces/company-interface.ts
@@ -0,0 +1,15 @@
+/**
+ * @description
+ * "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+ * @export
+ * @interface Company
+ */
+export interface Company {
+    name: String;
+    catchPhrase: String;
+    bs: String;
+}
