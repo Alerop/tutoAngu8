2842e832dc6352efa413ddf96aa0e4c7a5512d04
Alejandro Ladislao Rodriguez Perez <arodpere@everis.com>
2020-02-26 18:00:16 +0100

    Added new components and style'shit'

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    WelcomePageComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomePageComponent
   ]
 })
 export class HomeModule { }
