fe6d8c0f2cd4cef0cf19ae46bd3d12154e240ae5
Alejandro Ladislao Rodriguez Perez <arodpere@everis.com>
2020-03-10 17:19:13 +0100

    include new angular table with filter, sort and pagination also include new temporal file to git ignore and WIP: doc

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 
 
@@ -11,7 +17,12 @@ import { WelcomePageComponent } from './components/welcome-page/welcome-page.com
     WelcomePageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   exports: [
     HomePageComponent
